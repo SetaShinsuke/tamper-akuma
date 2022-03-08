3825fd69955898e5a2eeac18352d18045756c95b
seta <seta_shinsuke@qq.com>
2022-03-08 13:15:13 +0800

    v0.2

--- scripts/weibo-search-open.js
+++ scripts/weibo-search-open.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WBSearch
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  中键打开搜索结果
 // @author       Akuma
 // @match        https://weibo.com/*
