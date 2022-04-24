0f2883433eb955403f12346f8a5445c7b5ae0323
seta <seta_shinsuke@qq.com>
2022-04-24 16:52:12 +0800

    防止bilibili直播暂停

---
+++ scripts/bilibili-live-alive.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Bilibili-live-alive
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  防止B站直播在后台自动暂停
+// @author       xfgryujk
+// @include      /https?:\/\/live\.bilibili\.com\/?\??.*/
+// @include      /https?:\/\/live\.bilibili\.com\/\d+\??.*/
+// @include      /https?:\/\/live\.bilibili\.com\/(blanc\/)?\d+\??.*/
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/scripts/bilibili-live-alive.js
+// @downloadURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/scripts/bilibili-live-alive.js
+// ==/UserScript==
+
+(function() {
+    console.log('Prevent sleeping script running...');
+    let realSetTimeout = window.setTimeout;
+    let realSetInterval = window.setInterval;
+    window.setTimeout = function (func, ...args) {
+        let code = func.toString();
+        if (code.indexOf('triggerSleepCallback') !== -1) {
+            console.log(code);
+            console.log('Prevent sleeping');
+            return null;
+        }
+        return realSetTimeout.call(this, func, ...args);
+    }
+    window.setInterval = function (func, ...args) {
+        let code = func.toString();
+        if (code.indexOf('triggerSleepCallback') !== -1) {
+            console.log(code);
+            console.log('Prevent sleeping');
+            return null;
+        }
+        return realSetInterval.call(this, func, ...args);
+    }
+})();
