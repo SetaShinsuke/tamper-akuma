f8eea848e7ab018a24c5112e76d240de33d1eca5
seta <seta_shinsuke@qq.com>
2022-08-23 17:42:48 +0800

    Reader页增加ord显示

--- scripts/bilibili/bm-ep-index.js
+++ scripts/bilibili/bm-ep-index.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BM-EP-index
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Hover to show episode index.
 // @author       Akuma
 // @match        https://manga.bilibili.com/detail/*
+// @match        https://manga.bilibili.com/mc*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        GM_xmlhttpRequest
 // @updateURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/scripts/bilibili/bm-ep-index.js
@@ -17,17 +18,35 @@ console.log('starting inject');
 
 (function () {
     'use strict';
-    window.addEventListener('load', (x) => {
-        console.log('window loaded');
-        try {
-            setTimeout(onReady, TIMEOUT);
-        } catch (e) {
-            console.error(e);
-        }
-    });
+    try {
+        setTimeout(onReady, TIMEOUT);
+    } catch (e) {
+        console.error(e);
+    }
 })();
 
 function onReady() {
+    if (document.location.pathname.includes('/detail')) {
+        addIndexToDetailPage();
+    } else {
+        addIndexToReaderPage();
+    }
+}
+
+function addIndexToReaderPage() {
+    var ord = 0;
+    var btns = document.querySelectorAll('.episode-list>div>button')
+    for (var i = 0; i < btns.length; i++) {
+        if (btns[i].classList.contains('selected')) {
+            ord = i + 1;
+        }
+    }
+    console.log('ord: ', ord);
+    document.querySelector('.info-hud.none-select.info-hud.p-absolute.info-layer').title = `Ord:${ord}`;
+    console.log('Index added to Reader Page.');
+}
+
+function addIndexToDetailPage() {
     // unsafeWindow.GM_xmlhttpRequest = GM_xmlhttpRequest;
     // unsafeWindow.GM_listValues = GM_listValues;
     var epList = document.querySelector('.episode-list');
@@ -40,7 +59,7 @@ function onReady() {
         i++;
         btnEp.title = `${i}`;
     });
-    console.log('Index added.');
+    console.log('Index added to Detail Page.');
 }
 
 function doPost() {
