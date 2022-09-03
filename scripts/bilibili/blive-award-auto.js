cc7150b3f17416e0a7c459aa01aa6736391794a6
seta <seta_shinsuke@qq.com>
2022-09-03 22:37:11 +0800

    自动领取blive

---
+++ scripts/bilibili/blive-award-auto.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         BliveAwardAuto
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  自动领取直播挂机奖励
+// @author       Akuma
+// @match        https://live.bilibili.com/*?auto_award=true
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/utils/utils.js
+// @updateURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/bilibili/blive-award-auto.js
+// @downloadURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/bilibili/blive-award-auto.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log('Starting inject...');
+    inject();
+})();
+
+function inject() {
+    // do stuff
+    runWhenLoaded(`div[class^="eventTaskItem_button"`, btnAccept => {
+        console.log(`自动领取奖励, btn_text: ${btnAccept.innerText}`);
+        autoClick(btnAccept);
+    });
+}
+
+function autoClick(btnAccept) {
+    if (btnAccept.innerText === '领取') {
+        btnAccept.click();
+        var timeout = 3_000;
+        console.log(`已领取, ${parseInt(timeout / 1_000)}s 后领取下一阶段`);
+        setTimeout(() => {
+            autoClick(btnAccept);
+        }, timeout);
+    } else {
+        console.log(`领取完毕, 即将关闭窗口...`);
+        var customWindow = window.open('', '_blank', '');
+        setTimeout(() => {
+            customWindow.close();
+        }, 5_000);
+
+    }
+}
