e4ef8a5c453798b49f5e06d40d29dd151cb75e58
seta <seta_shinsuke@qq.com>
2023-04-26 22:56:50 +0800

    jconsole

--- scripts/javs/jav-enable-console.js
+++ scripts/javs/jav-enable-console.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jav-EnableConsole
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Stop clearing console!!
 // @author       Akuma
 // @match        https://jav.guru/*
@@ -12,6 +12,7 @@
 // @match        https://javsky.tv/*
 // @match        https://javcl.me/*
 // @match        https://javlove.club/*
+// @match        https://sbchill.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/SetaShinsuke/tamper-akuma/master/scripts/javs/jav-enable-console.js
@@ -21,8 +22,17 @@
 (function () {
     'use strict';
     console.log('Starting to enable console');
-    devtoolIsOpening = function stopIt() {
+    let stopper = function stopIt() {
         // 阻止清空 console
         console.log('Console clearing canceled.');
     };
+
+    switch (window.location.hostname) {
+        case 'sbchill.com':
+            check = stopper;
+            break
+        default:
+            devtoolIsOpening = stopper;
+    }
+
 })();
